0622f8243d3ae5f4d0b4d9d55c7d7463b282a7fa
Dory <dorybaga@gmail.com>
2017-09-16 09:58:53 -1000

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+/*jshint esversion: 6 */
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});

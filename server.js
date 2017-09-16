f7924196268a41076e2eb0d72857cefaf24d17f9
Dory <dorybaga@gmail.com>
2017-09-16 11:20:35 -1000

    created app module; renders to browser successfully

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+app.get('*', (req, res) => {
+  res.sendFile('index.html', {root: path.join(__dirname, '/public') });
+});
 app.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
 });

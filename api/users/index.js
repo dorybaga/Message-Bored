bfc5f723afb73d1c26701b47d6f44e55a428a0c1
Dory <dorybaga@gmail.com>
2017-09-16 15:06:14 -1000

    refactored users routes to use router.route

--- api/users/index.js
+++ api/users/index.js
@@ -5,37 +5,72 @@ const router = express.Router();
 const db = require('../../models');
 const Users = db.Users;
 
-app.get('/users', (req, res) => {
-  Users.findAll()
-  .then((names) => {
-    res.json(names);
+router.route('/users')
+  .get((req, res) => {
+    Users.findAll()
+    .then((names) => {
+      res.json(names);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   })
-  .catch((err) => {
-    console.log(err);
+  .post((req, res) => {
+    Users.create({
+      name: req.body.name
+    })
+    .then((name) => {
+      res.json(name);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   });
-});
 
-app.post('/users', (req, res) => {
-  Users.create({
-    name: req.body.name
-  })
-  .then((name) => {
-    res.json(name);
-  })
-  .catch((err) => {
-    console.log(err);
+router.route('/users/:id')
+  .get((req, res) => {
+    Users.findById(parseInt(req.params.id))
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
   });
-});
 
-app.get('/users/:id', (req, res) => {
-  Users.findById(parseInt(req.params.id))
-  .then((user) => {
-    res.json(user);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-});
+module.exports = router;
+
+// app.get('/users', (req, res) => {
+//   Users.findAll()
+//   .then((names) => {
+//     res.json(names);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+// });
+
+// app.post('/users', (req, res) => {
+//   Users.create({
+//     name: req.body.name
+//   })
+//   .then((name) => {
+//     res.json(name);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+// });
+
+// app.get('/users/:id', (req, res) => {
+//   Users.findById(parseInt(req.params.id))
+//   .then((user) => {
+//     res.json(user);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+// });
 
 
-module.exports = app;
+// module.exports = app;
